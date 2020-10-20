07e1cc8407f55820f2b76cde8b8c1c8676ba02a9
g00387822 <G00387822@gmit.ie>
2020-10-20 14:39:59 +0100

    have created a Python/Flask file app.py, when running Flask this file loads my-form.html and takes data input from the user and applies linear regression code from sklearn.linear_model, to calculate energy generated by wind speed

--- app.py
+++ app.py
@@ -1,3 +1,29 @@
+import sklearn.linear_model as lin
+import pandas as pd
+
+# Load a dataset.
+powerproduction = pd.read_csv('powerproduction.csv')
+
+def f(speed, p):
+    return p[0] + speed * p[1]
+
+def predict_power_output(speed):
+    return f(speed, p)
+
+speed = powerproduction["speed"].to_numpy()
+y = powerproduction["power"].to_numpy()
+
+speed = speed.reshape(-1, 1)
+
+model = lin.LinearRegression()
+model.fit(speed, y)
+r = model.score(speed, y)
+p = [model.intercept_, model.coef_[0]]
+
+
+
+
+
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -9,9 +35,6 @@ def my_form():
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
-    processed_text = text.upper()
-    return processed_text
+    return str(predict_power_output(int(text)))
 
 
-
-#https://stackoverflow.com/questions/51006831/adding-two-numbers-in-flask
