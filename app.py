5968169bd2b85dd7ccbd3d8dd61f8496da187374
g00387822 <G00387822@gmit.ie>
2020-11-29 07:07:32 +0000

    a clean up of files, modification of app.py to publish html and graph from flask

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 import sklearn.linear_model as lin
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
 
 # Load a dataset.
 powerproduction = pd.read_csv('powerproduction.csv')
+
+#https://github.com/ianmcloughlin/jupyter-teaching-notebooks/blob/master/models.ipynb
+#https://stackoverflow.com/questions/30336324/seaborn-load-dataset
+#https://chartio.com/resources/tutorials/how-to-save-a-plot-to-a-file-using-matplotlib/
+
+
+
+
+#https://stackoverflow.com/questions/32244753/how-to-save-a-seaborn-plot-into-a-file
+sns_plot = sns.pairplot(powerproduction)
+sns_plot.savefig('static/images/plot.png')
+
 
 def f(speed, p):
     return p[0] + speed * p[1]
@@ -35,6 +52,76 @@ def my_form():
 @app.route('/', methods=['POST'])
 def my_form_post():
     text = request.form['text']
-    return str(predict_power_output(int(text)))
+    return """ 
+    <style>
+    table.blueTable {
+  border: 1px solid #1C6EA4;
+  background-color: #EEEEEE;
+  width: 100%;
+  text-align: left;
+  border-collapse: collapse;
+}
+table.blueTable td, table.blueTable th {
+  border: 1px solid #AAAAAA;
+  padding: 3px 2px;
+}
+table.blueTable tbody td {
+  font-size: 13px;
+}
+table.blueTable tr:nth-child(even) {
+  background: #D0E4F5;
+}
+table.blueTable thead {
+  background: #1C6EA4;
+  background: -moz-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  background: -webkit-linear-gradient(top, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  background: linear-gradient(to bottom, #5592bb 0%, #327cad 66%, #1C6EA4 100%);
+  border-bottom: 2px solid #444444;
+}
+table.blueTable thead th {
+  font-size: 15px;
+  font-weight: bold;
+  color: #FFFFFF;
+  border-left: 2px solid #D0E4F5;
+}
+table.blueTable thead th:first-child {
+  border-left: none;
+}
 
+table.blueTable tfoot {
+  font-size: 14px;
+  font-weight: bold;
+  color: #FFFFFF;
+  background: #D0E4F5;
+  background: -moz-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+  background: -webkit-linear-gradient(top, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+  background: linear-gradient(to bottom, #dcebf7 0%, #d4e6f6 66%, #D0E4F5 100%);
+  border-top: 2px solid #444444;
+}
+table.blueTable tfoot td {
+  font-size: 14px;
+}
+table.blueTable tfoot .links {
+  text-align: right;
+}
+table.blueTable tfoot .links a{
+  display: inline-block;
+  background: #1C6EA4;
+  color: #FFFFFF;
+  padding: 2px 8px;
+  border-radius: 5px;
+}
+</style>   
+    
+    
+    <table class="blueTable"><thead><tr><th>head1</th></tr></thead><tbody><tr><td>""" + str(predict_power_output(int(text)))  + """ </td></tr></tbody></tr></table>
+    <img src="static/images/plot.png" alt="A Matplotlibplot">
+    <button onclick="goBack()">Go Back</button>
+
+<script>
+function goBack() {
+  window.history.back();
+}
+</script>
+    """ 
 
